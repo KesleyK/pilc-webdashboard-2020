634346e521322de38cc4949d712526811d64cfe6
Pedro Pessoa <pedropessoaramos@gmail.com>
2020-11-28 11:47:56 -0300

    react-router-dom

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+
 import Home from '../components/Home';
 import VideoCarousel from '../components/VideoCarousel';
 import Exercises from '../components/Exercises';
@@ -6,16 +8,25 @@ import Ranking from '../components/Ranking';
 
 import { useSelector } from 'react-redux';
 
-const Routes = () => {
-    const page = useSelector(state => state.page)
-    switch(page){
-        case 'home':
-            return <Home />;
-        case 'exercises':
-            return <Exercises />;
-        default:
-            return <Home />;
-    }
+const PriveteRoute = ({component : Component, ...restProps}) => {
+    const userToken = useSelector((state) => state.user.token);
+    console.log('user logado:'+(userToken))
+    return(
+        <Route {...restProps} render={props => (
+            (userToken || localStorage.getItem("token"))? 
+            (<Component {...props}/>):
+            (<Redirect to={{ pathname: '/', state: {from: props.location}  }} />)
+        )} />
+    )
 }
+
+const Routes = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={Home}/>
+            <PriveteRoute exact path="/exercicios" component={Exercises}/>
+        </Switch>
+    </BrowserRouter>
+)
 
 export default Routes;
