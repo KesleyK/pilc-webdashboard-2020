24e04914814f6629b2b42868574f5662e2564a3b
Pedro Pessoa <pedropessoaramos@gmail.com>
2020-12-15 18:00:20 -0300

    .

--- src/components/Turmas/index.js
+++ src/components/Turmas/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import useStyles from "./styles";
 import axios from "../../services/axios-instance";
 import { useSelector } from "react-redux";
@@ -6,7 +6,24 @@ import { useSelector } from "react-redux";
 const Turmas = () => {
     const classes = useStyles();
     const user = useSelector((state) => state.user);
+    const [turmas, setTurmas] = useState([]);
 
-    return <h1>Turmas</h1>;
+    const carregarTurmas = () =>{
+        let body = new FormData();
+        body.append("class", "Turma");
+        body.append("function", "getTurmasProfessor");
+        body.append("professor", user.matricula);
+        axios.post('/', body).then(res=>{
+            if(res.data.status == 'sucesso'){
+                setTurmas(res.data.dados);
+                console.log(res.data.dados)
+            }
+        }).catch(err =>{
+            console.log(err)
+        })
+    }
+
+    useEffect(carregarTurmas,[]);
+    return <h1>Turmas {JSON.stringify(turmas)}</h1>;
 }
 export default Turmas;
