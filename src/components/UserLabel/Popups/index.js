98cd1b73b443d49578c366ee6669fe685c97036c
Pedro Pessoa <pedropessoaramos@gmail.com>
2020-12-14 22:51:06 -0300

    .

---
+++ src/components/UserLabel/Popups/index.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import axios from "../../../services/axios-instance";
+
+
+export const MudarSenha = () => {
+    return (
+        <h1>MudarSenha</h1>
+    );
+}
+export const AdicionarAluno = () => {
+    return (
+        null
+    );
+}
+export const AdicionarProfessor = () => {
+    return (
+        null
+    );
+}
+export const AdicionarTurma = () => {
+    return (
+        null
+    );
+}
+export const AdicionarAlunoTurma = () => {
+    return (
+        null
+    );
+}
