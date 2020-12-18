35b89dcb3a34435b1df59292ccb29936154d0877
Pedro <pedropessoaramos@gmail.com>
2020-12-18 00:31:59 -0300

    .

--- src/components/Turma/LineChart/index.js
+++ src/components/Turma/LineChart/index.js
@@ -4,21 +4,20 @@ import { Bar } from 'react-chartjs-2';
 const LineChart = (props) => {
     console.log(props.relatorio)
     const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+    let arrCores = props.relatorio.map(el=>`rgba(${randomBetween(0,255)},${randomBetween(0,255)},${randomBetween(0,255)},1)`)
     const data = {
         labels: props.relatorio.map(el=>el.nome),
-        datasets: props.relatorio.map((el)=>{
-            console.log(el.pontos)
-            let color = [randomBetween(0,255),randomBetween(0,255)]
-            return ({
-                label: el.matricula,
-                backgroundColor: `rgba(${color[0]},${color[1]},132,0.2)`,
-                borderColor: `rgba(${color[0]},${color[1]},132,1)`,
-                borderWidth: 1,
-                hoverBackgroundColor: `rgba(${color[0]},${color[1]},132,0.4)`,
-                hoverBorderColor: `rgba(${color[0]},${color[1]},132,1)`,
-                data: el.pontos
-            })
-        })
+        datasets: [
+          {
+            label: 'Turma',
+            backgroundColor: arrCores,
+            borderColor: arrCores,
+            borderWidth: 1,
+            hoverBackgroundColor: arrCores,
+            hoverBorderColor: arrCores,
+            data: props.relatorio.map(el=>parseInt(el.pontos))
+          }
+        ]
       };
     return (
         <Bar 
